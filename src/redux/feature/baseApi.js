7adfc192263e29e50caeefa5b96acbb6f36735f5
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-10-13 15:19:07 +0600

    update base api

--- src/redux/feature/baseApi.js
+++ src/redux/feature/baseApi.js
@@ -2,10 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setAccessToken, setAdmin } from './auth/authSlice';
 
 const rawBaseQuery = fetchBaseQuery({
-    // baseUrl: 'http://10.10.20.70:4000',
-    // baseUrl: 'http://10.10.20.9:4000',
-    // baseUrl: 'http://192.168.0.103:4000',
-    // baseUrl: 'https://rnj64vmh-4000.inc1.devtunnels.ms',
     baseUrl: 'https://api.bankybondy.com',
 
     prepareHeaders: (headers, { getState }) => {
